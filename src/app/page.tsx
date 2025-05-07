215c7061567ee8cd18d3b4dc394dc783c1fb236f
fallenreaper <wllm.francis@gmail.com>
2025-05-07 08:47:27 -0400

    added DB use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const sampleUrls = [
   "https://kk46uxu9iv.ufs.sh/f/M9uN7NbtyqZUj1nwg8sGQZpsu8qcXil9boSP4K0FAwnCBHrh",
@@ -11,10 +12,22 @@ const mockImages = sampleUrls.map( (url, idx) => ({
   id: idx+1,
   url
 }))
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log("posts", posts);
   return (
-    <main className="bg-black text-white">
+    <main>
       <div className="flex flex-wrap gap-4">
+        { posts.map ( (post) => (
+          <div key={post.id} className="w-48">
+            <Link href={`/posts/${post.id}`}>
+              <div className="bg-white text-black p-4 rounded-lg">
+                {post.name}
+              </div>
+            </Link>
+          </div>
+        ))}
         { mockImages.map( (img) => (
           <div key={img.id} className="w-48">
             <img src={img.url} alt="image" />

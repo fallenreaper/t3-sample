e82723361ea3ccf8d81102c61ab5865dd7554723
fallenreaper <wllm.francis@gmail.com>
2025-05-13 21:56:07 -0400

    move the Sonnar toast to a nondefault directory to match components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import type React from "react";
-import { Toaster } from "~/components/ui/sonner";
+import { Toaster } from "~/app/_components/sonner";
 
 export const metadata: Metadata = {
   title: "Just a Sample Gallery",
